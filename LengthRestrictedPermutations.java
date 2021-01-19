c1fbd0c0ec57512877c12195f33d086147be90c8
Banee Ishaque K <baneeishaque@hotmail.com>
2021-01-19 09:39:39 +0000

    Main Class

---
+++ LengthRestrictedPermutations.java
@@ -0,0 +1,24 @@
+class LengthRestrictedPermutations {
+
+    private static void permutation(final char[] perm, final int pos, final String str) {
+
+        if (pos == perm.length) {
+        
+            System.out.println(new String(perm));
+        
+        } else {
+        
+            for (int i = 0; i < str.length(); i++) {
+        
+                perm[pos] = str.charAt(i);
+                permutation(perm, pos + 1, str);
+            }
+        }
+    }
+
+    public static void main(final String[] args) throws java.lang.Exception {
+
+        final char[] perm = new char[2];
+        permutation(perm, 0, "123");
+    }
+}
